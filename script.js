a5f393bea6c7b467298ba21c4dd5885f03be0b7a
Morris K.N <momalley042@gmail.com>
2023-06-22 19:15:57 +0300

    Done script

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-// const limitInput = document.getElementById('itemsLimit');
-// limitInput.addEventListener('change', (e) => {
-//     e.preventDefault();
-//     const UserLimit = limitInput.value;
+var itemCount = 0;
+var itemsContainer = document.getElementById("itemsContainer");
+var loadMoreBtn = document.getElementById("loadMoreButton");
+const defItem = `<div class="col-3 card" id="singleItem">
+      <img src="Images/gloveCycling-2.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      </div>`;
+
+const userInput = document.getElementById('itemsLimit');
+userInput.addEventListener('change', (e) => {
+    e.preventDefault()
     
-//     const itemsContainer = document.querySelector('.itemsContainer');
-//     const computedStyle = getComputedStyle(itemsContainer);
-//     const defaultHeight = computedStyle.getPropertyValue('max-height');
-    
-//     const singleItem = document.querySelectorAll('#singleItem');
-//     singleItem.forEach((item) => {
-//         const itemStyles = getComputedStyle(item);
-//         const defaultItemHeight = itemStyles.getPropertyValue('height');
+    const itemsToShow = userInput.value;
+    loadItems(itemsToShow);
 
-        
-//     })
-//     itemsContainer.style.maxHeight = UserLimit * defaultItemHeight;
-//     console.log(itemsContainer.style.maxHeight = UserLimit * defaultItemHeight)
+    loadMoreBtn.addEventListener("click", function () {
+        loadItems(itemsToShow); 
+      });
+      
+      function loadItems(count) {
+        for (var i = 0; i < count; i++) {
+          var item = createItem(defItem);
+          itemsContainer.appendChild(item);
+          itemCount++;
+        }
+      
+        if (itemCount >= 20) {
+          loadMoreBtn.style.display = "none"; // Hide the button when all items are loaded
+        }
+      }
+      
+      function createItem(htmlString) {
+        var item = document.createElement("div");
+        item.innerHTML = htmlString.trim();
+        return item.firstChild;
+      }
+})
 
-// })
-
-const itemsContainer = document.querySelector('.itemsContainer');
-const computedStyle = getComputedStyle(itemsContainer);
-const loadMoreButton = document.getElementById('loadMoreButton');
-
-loadMoreButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    let computedStyle = window.getComputedStyle(itemsContainer);
-    let currentMaxHeight = parseInt(computedStyle.getPropertyValue('max-height'));
-    
-    if ((currentMaxHeight + 100) > window.innerHeight) {
-      return false;
-    } else {
-      itemsContainer.style.maxHeight = `${currentMaxHeight + 500}px`;
-    }
-  });
-  
